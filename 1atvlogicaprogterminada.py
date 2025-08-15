46404c8ce046f9865cb21f93aa2b2e7763461569
wesley8266 <wesleydasilvaalmeida706@gmail.com>
2025-08-15 12:37:57 -0300

    lista 1 python

---
+++ 1atvlogicaprogterminada.py
@@ -0,0 +1,60 @@
+#1. for i in range(1,11):
+#    print(f"número: {i}")
+
+
+#2. N = 10
+#while N != 0:
+#    print(f"número: {N}")
+#    N -= 1
+
+#3. Nome = input("insira seu nome:")
+#for i in range (5):
+#    print(f"nome: {Nome}")
+
+#4. for i in range(0,22,2):
+#    print(f"número: {i}")
+
+#5.
+# senha = "1234"
+# while True:
+#    correta = input("insira a senha:")
+#    if senha == correta :
+#     print("senha correta, bem-vindo")
+#     break
+#    else:
+#      print('Senha incorreta')
+
+#6
+# N = 0
+# for i in range (5):
+#    n= float(input(f"insira o {i+1}° número: "))
+#    N += n
+# print(f"a soma é: {N} ")    
+
+#7. 
+# i = 0
+# while i <= 10:
+#    i += 1
+#    if i % 2 != 0:
+#         continue
+#    print(i)
+
+#8.
+# cont = 0
+# while True:
+#     user = input('Digite um número: ').strip()
+#     if user == '0':
+#         print(cont)
+#         break
+#     cont += 1
+
+    
+#9.
+# Numero = int(input("insira o número: "))
+# for i in range (1,11):
+#    resultado = Numero * i
+#    print(f"Número: {Numero} * {i} = {resultado} ")
+    
+    
+    
+    
