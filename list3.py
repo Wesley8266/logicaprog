eafd53b0322a7e61e958eb6df0b9fcf769874e42
Wesley8266 <wesleydasilvaalmeida706@gmail.com>
2025-08-22 10:18:35 -0300

    Add files via upload

---
+++ list3.py
@@ -0,0 +1,24 @@
+# peso = float(input("insira o peso: "))
+# altura = float(input("insira a sua altura em metros: "))
+# imc = peso / (altura ** 2)
+# if imc < 18.5:
+#     print(f"{imc} abaixo do peso")
+# elif imc <= 24.9:
+#     print(f"{imc} peso normal")
+# elif imc <= 29.9:
+#     print(f"{imc} sobrepeso")
+# else :
+#     print(f"{imc} obeso")
+
+
+# n = int(input("insira um numero inteiro: "))
+# for i in range(n):
+#     if i % 2 == 0:
+#         print(f"{i} é par")
+#     else :
+#         print(f"{i} é impar")
+
+# Numero = int(input("insira o número: "))
+# for i in range (1,11):
+#    resultado = Numero * i
+#    print(f"Número: {Numero} * {i} = {resultado} ")
