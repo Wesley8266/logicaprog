f696790420fdf82949386cb00ba4353316268577
unknown <wesleydasilvaalmeida706@gmail.com>
2025-10-20 15:01:35 -0300

    calc

---
+++ calculadora/operacoes.py
@@ -0,0 +1,11 @@
+def soma(a,b):
+    return a + b
+def subtraçao(a,b):
+    return a - b
+def multiplicacao(a,b):
+    return a * b
+def divisao(a,b):
+    if b == 0:
+        print("não é possivel dividir por 0")
+    else:
+        return a / b
