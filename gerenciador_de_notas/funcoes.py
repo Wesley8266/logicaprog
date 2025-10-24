0d0d370532e6109f8d41af504bdfa17bd6715f00
unknown <wesleydasilvaalmeida706@gmail.com>
2025-10-24 09:09:59 -0300

    dsqfwef

--- gerenciador_de_notas/funcoes.py
+++ gerenciador_de_notas/funcoes.py
@@ -1,10 +1,11 @@
-def Lista_Notas(ListaNotas):
+def calcular_media(ListaNotas):
     media = sum(ListaNotas) / len (ListaNotas)
     return media
+
 def verificar_situacao(media):
     if media >= 7:
         return "aprovado"
     elif media >= 5 and media < 7:
         return "recuperação"
     else:
-        return "Reprovado"
+        return "Reprovado" 
--- gerenciador_de_notas/programa.py
+++ gerenciador_de_notas/programa.py
@@ -1,23 +1,26 @@
 from funcoes import *
 
 ListaNotas = []
+notaDict = {}
+
 print("insira 1 para cadatrar aluno")
 print("insira 2 para exibir relatório de aluno")
 print("insira 0 para sair do programa")
+
 while True:
     opcao = input("qual procedimento deseja realizar? ")
     if opcao == "1":
-        print("cadastro")
+        print("cadastro: ")
         nome = input("nome do aluno: ")
+
         for i in range(3):
-            notas = int(input("notas do aluno: "))
+            notas = float(input("notas do aluno: "))
             ListaNotas.append(notas)
     elif opcao == "2":
         print(f"nome do aluno: {nome}")
         print(f"Notas: {ListaNotas}")
-        print("media:", Lista_Notas(ListaNotas))
-
-
+        print(f"media: {calcular_media(ListaNotas)}")
+        print(f"situaçao: {verificar_situacao(calcular_media)}")
         
     
 
