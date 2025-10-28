c51d7badbbf74188491f62fdd70e65ec2f52894b
Lunafeitas <luna.freitas5@aluno.ce.gov.br>
2025-10-28 11:07:59 -0300

    lista3

---
+++ biblioteca/funcoes.py
@@ -0,0 +1,22 @@
+def adicionarlivro(listalivros):
+    titulo=input("Digite o título do  livro: ")
+    autor=input("Digite o autor do livro: ")
+    status="Disponível"
+    livro={"titulo": titulo, "autor": autor, "status": status}
+    listalivros.append(livro)
+    return listalivros
+
+def exibirlivros(listalivros):
+    return listalivros
+
+def emprestarlivro(listalivros):
+    # livro = input("qual livro deseja emprestar: ")
+    # for i in listalivros:
+    #      if livro in listalivros["titulo"]:
+
+    
+def devolverlivro(listalivros):
+    status="Disponível"
+
+
+
